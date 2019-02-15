b81add35067475b46473b43a7e629fef05fa9bf6
SedMek <32764135+SedMek@users.noreply.github.com>
2019-02-15 15:44:31 +0100

    'V1'

--- speed_test.py
+++ speed_test.py
@@ -3,6 +3,10 @@ import time
 import matplotlib.pyplot as plt
 from statistics import mean
 from datetime import datetime
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 
 
 def main():
@@ -46,6 +50,15 @@ def to_datetime(timestamp):
     return datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
 
 
+def to_time(timestamp):
+    """
+    converts an integer timestamp to a string date
+    :param timestamp: integer timestamp in seconds
+    :return: string date under format %Y-%m-%d %H:%M:%S
+    """
+    return datetime.utcfromtimestamp(timestamp).strftime('%H:%M')
+
+
 def save_complaint(start_time, end_time, trigger_value, mean_value, filename='complaint.txt'):
     """
     Stores a complaint start_time + ';' + end_time + ';' + str(trigger_value) + ';' + str(mean_value) in a local text file
@@ -65,8 +78,11 @@ def prepare_complaint(time_list, down_list, value_trigger, duration_trigger):
     end_index = len(time_list) - 1
     is_down = False
     complaint_counter = 0
+    total_down = 0
     for i in range(len(down_list)):
-        print('------------- ', i, is_down, ' ------------')
+        # print('------------- ', i, is_down, ' ------------')
+        if down_list[i] <= value_trigger:
+            total_down += 1
         if down_list[i] <= value_trigger and not is_down:
             is_down = True
             start_index = i
@@ -74,12 +90,13 @@ def prepare_complaint(time_list, down_list, value_trigger, duration_trigger):
             is_down = False
             end_index = i
             if time_list[end_index] - time_list[start_index] > duration_trigger:
-                print(start_index, end_index)
-                print(down_list[start_index:end_index])
+                # print(start_index, end_index)
+                # print(down_list[start_index:end_index])
                 save_complaint(to_datetime(time_list[start_index]), to_datetime(time_list[end_index]), value_trigger,
                                mean(down_list[start_index:end_index]))
                 complaint_counter += 1
     print("Finished complaint report! {} complaint(s) were issued.".format(complaint_counter))
+    return complaint_counter, total_down
 
 
 def download_info(frequency=10, duration_covered=300, runs=5):
@@ -94,6 +111,7 @@ def download_info(frequency=10, duration_covered=300, runs=5):
     time_list = []
     time_formatted_list = []
     for i in range(round(duration_covered / frequency)):
+        print('------------- run {} -----------'.format(i))
         time_list.append(round(time.time()))
         down_list.append(to_mbps(st.download()))
         time.sleep(frequency)
@@ -101,26 +119,71 @@ def download_info(frequency=10, duration_covered=300, runs=5):
 
 
 def download_graph(time_list, down_list, value_trigger):
-    fig = plt.figure()
+    fig = plt.figure(figsize=(9, 2))
     plt.plot(time_list, down_list, 'b', time_list, [value_trigger for i in time_list], 'r')
-    fig.savefig('plots/{}.png'.format(to_datetime(time_list[0]).replace(':','-')))
+    # print(plt.xticks())
+    plt.xlabel('heures')
+    plt.ylabel('débit (Mbps)')
+    plt.xticks([time_list[0], time_list[int(len(time_list) / 2)], time_list[-1]],
+               [to_time(time_list[0]), to_time(time_list[int(len(time_list) / 2)]), to_time(time_list[-1])])
+    fig.savefig('fig.png'.format(to_datetime(time_list[0]).replace(':', '-')))
 
 
 def down():
-    time_list, down_list = download_info(frequency=60, duration_covered=1800, runs=5)
+    frequency = 60
+    time_list, down_list = download_info(frequency=frequency, duration_covered=1200, runs=5)
     save_records(time_list, down_list)
-    value_trigger = 500
+    value_trigger = 600
     duration_trigger = 120
     download_graph(time_list, down_list, value_trigger)
-    prepare_complaint(time_list, down_list, value_trigger, duration_trigger)
+    complaint_counter, total_down = prepare_complaint(time_list, down_list, value_trigger, duration_trigger)
+    if complaint_counter > 0:
+        send_password_mail("sedmekipest@gmail.com", '0000', value_trigger, frequency, total_down, time_list)
+
+
+def send_password_mail(user_mail, contract_number, value_trigger, frequency, total_down, time_list):
+    gmail_user = "hetcompte@gmail.com"
+    gmail_pwd = "08587428aqwzsx"
+    TO = user_mail
+    SUBJECT = "Réclamation débit faible - Contrat n:{}".format(contract_number)
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(gmail_user, gmail_pwd)
+    # BODY = '\r\n'.join(['To: %s' % TO,
+    #                     'From: %s' % gmail_user,
+    #                     'Subject: %s' % SUBJECT,
+    #                     '', TEXT]).encode('utf-8')
+    msgRoot = MIMEMultipart('related')
+    msgRoot['Subject'] = SUBJECT
+    msgRoot['From'] = gmail_user
+    msgRoot['To'] = user_mail
+    # Create the body of the message.
+    html = """Bonjour Monsieur/Madame,
+            <p>Je suis propriétaire du contrat n: {}. Je vous contacte suite à la baisse répétitive que je constate sur mon débit: le débit était inférieur à {}Mbps (alors que mon contrat est de 1000Mbps) pendant {}minutes entre {} et {} aujourd'hui comme illustré dans le graphe suivant:</p>
+            <img src='cid:image1'>
+            
+            <p>Veuillez fixer ce problème dans les délais les plus courts.
+            <p>Si cela ne vous est pas possible, je demande qu'on révise mon abonnement pour trouver un prix adapté au service médiocre que je reçois!
+            <p>Bien cordialement,
+            <p>Ghassen Hamdi \
+           """.format(contract_number, value_trigger, (frequency * total_down) // 60, to_time(time_list[0]),
+                      to_time(time_list[-1]))
+    # Record the MIME types.
+    msgHtml = MIMEText(html, 'html')
+    # add the image
+    image_file = 'fig.png'
+    img = open(image_file, 'rb').read()
+    msgImg = MIMEImage(img, 'png')
+    msgImg.add_header('Content-ID', '<image1>')
+    msgImg.add_header('Content-Disposition', 'inline', filename=image_file)
+    # attach both parts
+    msgRoot.attach(msgHtml)
+    msgRoot.attach(msgImg)
+    server.sendmail(gmail_user, [TO], msgRoot.as_string())
+    server.quit()
+    print('email sent')
 
 
 if __name__ == "__main__":
     down()
-
-    # time_list = [1545563002 + 60 * i for i in range(10)]
-    # down_list = [10, 9, 3, 2, 1, 7, 8, 4, 5, 6]
-    # down_list = [i*1024*1024 for i in down_list]
-    # print(to_datetime(time.time()), type(time.time()))
-    # prepare_complaint(down_list, time_list, 3, 60)
-    # pass
